0eb41e5621d37c20bf80067fb254e3b35dca8047
H.M.Touhid Mia <hmtm.cse@gmail.com>
2014-06-07 23:44:05 +0600

    Touhid Mia

---
+++ array/array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NAME[0]="Touhid"
+NAME[1]="Rifa"
+NAME[2]="Nabila"
+NAME[3]="Riddhi"
+NAME[4]="Bithi"
+
+MYFRND=("Rifa" "Nabila" "Riddhi")
+
+echo "First Index: ${NAME[0]}"
+echo "First Index: ${MYFRND[0]}"
