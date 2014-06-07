633590919efd9bd4fc3ece4ab06b020b5b444e05
H.M.Touhid Mia <hmtm.cse@gmail.com>
2014-06-07 23:30:15 +0600

    Touhid Mia

---
+++ variables/sVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "All Parameters: $@"
+echo "All Parameters: $*"
+echo "Number of Parameters : $#"
+echo "Current Process ID : $$"
